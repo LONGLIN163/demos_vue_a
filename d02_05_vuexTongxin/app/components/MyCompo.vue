2bf302bc34cd60ab0970de398c6b83b0246732e2
developerlin123 <developerlin123@gmail.com>
2021-08-31 16:46:05 +0200

    d

---
+++ d02_05_vuexTongxin/app/components/MyCompo.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        haha
+        <h1>count is : {{count}}</h1>
+        <button @click="addHandler">add</button>
+        <button @click="minusHandler">minus</button>
+    </div>
+</template>
+  
+<script>
+    //import store from "./store.js"
+    export default {
+        data(){
+            return {
+                m:6
+            };
+        },
+        methods:{
+          addHandler(){
+              //store.commit("add")
+              this.$store.commit("add")
+        },
+          minusHandler(){
+            //store.commit("minus")
+            this.$store.commit("minus")
+          }
+        },
+        computed:{
+            count(){
+                //return store.state.count
+                return this.$store.state.count
+            }
+        }
+    }
+</script>
+  
+<style>
+
+
+</style> 
